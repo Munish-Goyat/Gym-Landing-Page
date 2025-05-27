1893bf835143f7cd15cf958534bc1fd0e71624ee
Munish-Goyat <156228514+Munish-Goyat@users.noreply.github.com>
2025-05-27 22:38:05 +0530

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,16 +1,13 @@
 <?php
-$host = "localhost";  // Hostname (usually localhost)
-$username = "root";   // MySQL username (default is root)
-$password = "";       // MySQL password (leave blank for XAMPP default)
-$database = "pg_life"; // Name of your database
+$servername = "localhost";
+$username = "root"; // or your DB username
+$password = "";     // or your DB password
+$database = "gym_membership"; // replace with your DB name
 
-// Create a connection
-$conn = new mysqli($host, $username, $password, $database);
+$conn = mysqli_connect($servername, $username, $password, $database);
 
-// Check the connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connection successful!";
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 ?>
